9fbbccbb4cdc540f457a08b1a53055e81689ce9b
Adrian Cheoreanu <adrian.cheoreanu@magorcorp.com>
2016-11-01 13:40:13 -0400

    get users

--- app.js
+++ app.js
@@ -129,12 +129,14 @@ app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
 app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
+app.get('/users', userController.getUsers);
 app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
 
+app.get('/question', questionController.getQuestion);
 app.get('/question/:id', questionController.getQuestion);
 app.get('/question/create', questionController.getCreate);
 app.post('/question/create', questionController.postCreate);
